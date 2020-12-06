bedd030e99c36582f8c2aa3a998c2f420c1f15e0
Jeremy <jerec_11@yahoo.com.ar>
2020-12-06 11:15:30 -0300

    se modificó el titulo de cada juego

--- src/pages/games/GameNine.vue
+++ src/pages/games/GameNine.vue
@@ -1,5 +1,52 @@
 <template>
-    <h1>GAME </h1>
+  <v-row>
+    <v-col cols="12">
+      <v-card
+          class="mx-auto"
+          max-width="400">
+        <v-card-title class="justify-center">EL TREN</v-card-title>
+      </v-card>
+    </v-col>
+    <v-col cols="12">
+      <v-card
+          class="mx-auto"
+          max-width="500">
+      <h4>en esta seccion  van a estar las imagenes</h4>
+      </v-card>
+    </v-col>
+    <v-col cols="12">
+      <v-card
+          class="mx-auto"
+          max-width="173">
+        <v-card-actions>
+          <v-btn
+              class="mx-2"
+              fab
+              dark
+              large
+              color="cyan"
+              to="/"
+          >
+            <v-icon dark>
+              mdi-home
+            </v-icon>
+          </v-btn>
+          <v-btn
+              class="mx-2"
+              fab
+              dark
+              large
+              color="green"
+          >
+            <v-icon dark>
+              mdi-check
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+
 </template>
 <script>
 export default {
