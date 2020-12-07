f0c01fd9a1864cb088c181a3dfb64ef626a525fa
dracaster <lujanrojas.informatica@gmail.com>
2020-12-07 12:33:57 -0300

    agrego archivo de exportacion de imagense

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -3,23 +3,156 @@ import anana from '../assets/cards/anana.png'
 import anillo from '../assets/cards/anillo.png'
 import anteojos from '../assets/cards/anteojos.png'
 import arania from '../assets/cards/arania.png'
+import arbol from '../assets/cards/arbol.png'
 import aro from '../assets/cards/aro.png'
+import auto from '../assets/cards/auto.png'
+import elefante from '../assets/cards/elefante.png'
 import empanadas from '../assets/cards/empanadas.png'
+import escoba from '../assets/cards/escoba.png'
+import escuela from '../assets/cards/escuela.png'
 import espada from '../assets/cards/espada.png'
-import uno from '../assets/cards/uno.png'
+import espejo from '../assets/cards/espejo.png'
+import estante from '../assets/cards/estante.png'
+import estrella from '../assets/cards/estrella.png'
+import iglesia from '../assets/cards/iglesia.png'
+import iman from '../assets/cards/iman.png'
+import incendio from '../assets/cards/incendio.png'
+import indio from '../assets/cards/indio.png'
+import isla from '../assets/cards/isla.png'
+import jabon from '../assets/cards/jabon.png'
+import jamon from '../assets/cards/jamon.png'
+import jardinero from '../assets/cards/jardinero.png'
+import jarra from '../assets/cards/jarra.png'
+import jugo from '../assets/cards/jugo.png'
+import lapiz from '../assets/cards/lapiz.png'
+import lata from '../assets/cards/lata.png'
+import linterna from '../assets/cards/linterna.png'
+import mago from '../assets/cards/mago.png'
+import mano from '../assets/cards/mano.png'
+import manzana from '../assets/cards/manzana.png'
+import martillo from '../assets/cards/martillo.png'
+import media from '../assets/cards/media.png'
+import mesa from '../assets/cards/mesa.png'
+import mochila from '../assets/cards/mochila.png'
+import moneda from '../assets/cards/moneda.png'
+import mono from '../assets/cards/mono.png'
+import montania from '../assets/cards/montania.png'
+import moto from '../assets/cards/moto.png'
+import munieca from '../assets/cards/munieca.png'
+import murcielago from '../assets/cards/murcielago.png'
+import nariz from '../assets/cards/nariz.png'
+import nena from '../assets/cards/nena.png'
+import nido from '../assets/cards/nido.png'
+import nube from '../assets/cards/nube.png'
+import nudo from '../assets/cards/nudo.png'
+import ojo from '../assets/cards/ojo.png'
+import ojota from '../assets/cards/ojota.png'
 import ola from '../assets/cards/ola.png'
+import olla from '../assets/cards/olla.png'
+import oreja from '../assets/cards/oreja.png'
 import oso from '../assets/cards/oso.png'
+import oveja from '../assets/cards/oveja.png'
+import rana from '../assets/cards/rana.png'
+import raton from '../assets/cards/raton.png'
+import regalo from '../assets/cards/regalo.png'
+import reloj from '../assets/cards/reloj.png'
+import ropa from '../assets/cards/ropa.png'
+import rueda from '../assets/cards/rueda.png'
+import sapo from '../assets/cards/sapo.png'
+import sarten from '../assets/cards/sarten.png'
+import semaforo from '../assets/cards/semaforo.png'
+import serpiente from '../assets/cards/serpiente.png'
+import sifon from '../assets/cards/sifon.png'
+import sillon from '../assets/cards/sillon.png'
+import sol from '../assets/cards/sillon.png'
+import sombrero from '../assets/cards/sombrero.png'
+import suma from '../assets/cards/suma.png'
+import tambor from '../assets/cards/tambor.png'
+import unia from '../assets/cards/unia.png'
+import uniforme from '../assets/cards/uniforme.png'
+import uno from '../assets/cards/uno.png'
+import utiles from '../assets/cards/utiles.png'
+import uva from '../assets/cards/uva.png'
+import venda from '../assets/cards/venda.png'
+import volcan from '../assets/cards/volcan.png'
+
 
 export {
-    aro,
     ala,
     anana,
     anillo,
     anteojos,
+    arbol,
     arania,
+    aro,
+    auto,
+    elefante,
     empanadas,
+    escoba,
+    escuela,
     espada,
+    espejo,
+    estante,
+    estrella,
+    iglesia,
+    iman,
+    incendio,
+    indio,
+    isla,
+    jabon,
+    jamon,
+    jardinero,
+    jarra,
+    jugo,
+    lapiz,
+    lata,
+    linterna,
+    mago,
+    mano,
+    manzana,
+    martillo,
+    media,
+    mesa,
+    mochila,
+    moneda,
+    mono,
+    montania,
+    moto,
+    munieca,
+    murcielago,
+    nariz,
+    nena,
+    nido,
+    nube,
+    nudo,
+    ojo,
+    ojota,
     ola,
+    olla,
+    oreja,
     oso,
-    uno
+    oveja,
+    rana,
+    raton,
+    regalo,
+    reloj,
+    ropa,
+    rueda,
+    sapo,
+    sarten,
+    semaforo,
+    serpiente,
+    sifon,
+    sillon,
+    sol,
+    sombrero,
+    suma,
+    tambor,
+    unia,
+    uniforme,
+    uno,
+    utiles,
+    uva,
+    venda,
+    volcan
 }
