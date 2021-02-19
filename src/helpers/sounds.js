de497b84d48f9b65596624caf93019ace134f53e
dracaster <lujanrojas.informatica@gmail.com>
2021-02-18 21:20:59 -0300

    agrego sonidos de las letras

--- src/helpers/sounds.js
+++ src/helpers/sounds.js
@@ -13,6 +13,22 @@ import gameTitleOne from '../assets/sounds/gameTitles/game-one.wav'
 //Others
 import ganaste from '../assets/sounds/ganaste.wav'
 import error from '../assets/sounds/error.wav'
+
+//Letters
+import a from '../assets/sounds/letters/A.wav'
+import d from '../assets/sounds/letters/D.wav'
+import e from '../assets/sounds/letters/E.wav'
+import f from '../assets/sounds/letters/F.wav'
+import l from '../assets/sounds/letters/L.wav'
+import m from '../assets/sounds/letters/M.wav'
+import n from '../assets/sounds/letters/N.wav'
+import o from '../assets/sounds/letters/O.wav'
+import p from '../assets/sounds/letters/P.wav'
+import r from '../assets/sounds/letters/R.wav'
+import s from '../assets/sounds/letters/S.wav'
+import t from '../assets/sounds/letters/T.wav'
+import u from '../assets/sounds/letters/U.wav'
+
 export {
     espadaSound,
     unoSound,
@@ -23,5 +39,18 @@ export {
     alaSound,
     gameTitleOne,
     ganaste,
-    error
+    error,
+    a,
+    d,
+    e,
+    f,
+    l,
+    m,
+    n,
+    o,
+    p,
+    r,
+    s,
+    t,
+    u
 }
--- src/pages/games/GameOne/GameOnePartTwo.vue
+++ src/pages/games/GameOne/GameOnePartTwo.vue
@@ -1,42 +1,66 @@
 <template>
-  <v-row>
-    <Header title="ARMA LAS PALABRAS"/>
-     <TableImages :items="letters" cols="2" maxWidth="410"/>
-
-
-      <v-col cols="4" v-for="image in images" :key="image.id">
-        <v-card>
-        <img :src="image.url" class="item">
-        <table id="image.letter" class="tablas">
-          <tr>
-            <td>
-              <div class="box-image"></div>
-            </td>
-            <td>
-              <div class="box-image"></div>
-            </td>
-            <td>
-              <div class="box-image"></div>
-            </td>
-          </tr>
-        </table>
+  <v-container>
+    <v-row>
+      <Header title="ARMA LAS PALABRAS"/>
+      <v-col cols="12">
+        <v-card
+            class="mx-auto background"
+           >
+          <v-row>
+            <v-col
+                v-for="item in letters"
+                :key="item.id"
+                :cols="$vuetify.breakpoint.xs ? '4' : '2'"
+            >
+              <v-img
+                  @mouseover="imageAnimated = true"
+                  @mouseleave="imageAnimated = false"
+                  :src="item.url"
+                  :width="$vuetify.breakpoint.xs ? '50' : '100'"
+                  :class="{'paint': item.selected, 'slideDownBtn': imageAnimated}"
+                  v-on:click="
+                  selectItem(images,item);
+                  removePainted(item)"
+              >
+              </v-img>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
 
+      <v-col cols="4" v-for="image in images" :key="image.id">
+        <v-card>
+          <img :src="image.url" class="item">
+          <table id="image.letter" class="tablas">
+            <tr>
+              <td>
+                <div class="box-image"></div>
+              </td>
+              <td>
+                <div class="box-image"></div>
+              </td>
+              <td>
+                <div class="box-image"></div>
+              </td>
+            </tr>
+          </table>
+        </v-card>
+      </v-col>
 
-
-  </v-row>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import Header from "../../../components/Header/Header";
 import {a, ola, l, o, oso, r, u, s, uno} from '../../../helpers/images'
-import {olaSound, osoSound, unoSound} from "../../../helpers/sounds";
-import TableImages from "../../../components/TableImages/TableImages";
+import {olaSound, osoSound, unoSound,a as aSound, l as lSound, o as oSound, r as rSound,
+u as uSound, s as sSound} from "../../../helpers/sounds";
 
 export default {
-  components: {TableImages, Header},
+  components: {Header},
   data() {
     return {
+      sounds:[],
       letters: [
         {
           id: 0,
@@ -44,7 +68,7 @@ export default {
           letter: 'a',
           url: a,
           selected: false,
-          sound: ''
+          sound: aSound
         },
         {
           id: 1,
@@ -52,7 +76,7 @@ export default {
           letter: 'o',
           url: o,
           selected: false,
-          sound: ''
+          sound: oSound
         },
         {
           id: 2,
@@ -60,7 +84,7 @@ export default {
           letter: 'l',
           url: l,
           selected: false,
-          sound: ''
+          sound: lSound
         },
         {
           id: 3,
@@ -68,7 +92,7 @@ export default {
           letter: 'r',
           url: r,
           selected: false,
-          sound: ''
+          sound: rSound
         },
         {
           id: 4,
@@ -76,7 +100,7 @@ export default {
           letter: 'u',
           url: u,
           selected: false,
-          sound: ''
+          sound: uSound
         },
         {
           id: 5,
@@ -84,7 +108,7 @@ export default {
           letter: 'o',
           url: o,
           selected: false,
-          sound: ''
+          sound: oSound
         },
         {
           id: 6,
@@ -92,7 +116,7 @@ export default {
           letter: 'o',
           url: o,
           selected: false,
-          sound: ''
+          sound: oSound
         },
         {
           id: 7,
@@ -100,7 +124,7 @@ export default {
           letter: 's',
           url: s,
           selected: false,
-          sound: ''
+          sound: sSound
         },
         {
           id: 8,
@@ -108,7 +132,7 @@ export default {
           letter: 'o',
           url: o,
           selected: false,
-          sound: ''
+          sound: oSound
         },
       ],
       images: [
@@ -139,7 +163,26 @@ export default {
       ]
     }
   },
-  methods: {}
+  methods: {
+    generateSounds() {
+      let audio
+      this.letters.forEach(item => {
+        audio = new Audio(item.sound)
+        this.sounds.push({[item.title]: audio})
+      })
+    },
+    selectItem(items, itemSelected) {
+      itemSelected.selected = !itemSelected.selected
+      this.sounds[itemSelected.id][itemSelected.title].play()
+    },
+    removePainted(exceptedItem) {
+      return this.images.map(item => {
+        if (item.title !== exceptedItem.title) {
+          item.selected = false
+        }
+      })
+    }
+  }
 }
 </script>
 <style>
@@ -151,5 +194,12 @@ export default {
   border-bottom: 4px solid blue;
   width: 60px;
   height: 60px;
+}
+
+.paint {
+  border: 5px solid #e25b2a;
+}
+.background{
+  background-color: burlywood;
 }
 </style>
